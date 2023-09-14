5d915ed437a494e139f6282cdb3e938fc76d2912
John P <70922864+p134c0d3@users.noreply.github.com>
2023-09-13 23:21:58 -0500

    Update main.js

--- javascript-advanced-concepts/main.js
+++ javascript-advanced-concepts/main.js
@@ -20,36 +20,36 @@
 // console.log(user);
 
 function getUsers() {
-  return fetch('https://jsonplaceholder.typicode.com/users', {
-    method: 'GET',
-    headers: {"Content-Type": "application/json"}
-  }).then(res => res.json())
+	return fetch("https://jsonplaceholder.typicode.com/users");
 }
 
 function getUserPosts(userId) {
-  return fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json'}
-  }).then(res => res.json())
+	return getFetch("https://jsonplaceholder.typicode.com/posts", {
+		userId: userId,
+	});
 }
 
-getUsers().then(users => {
-  users.forEach(user => {
-    getUserPosts(user.id).then(posts => {
-      console.log(user.name);
-      console.log(posts.length);
-    })
-  })
-})
+// getUsers().then((users) => {
+// 	users.forEach((user) => {
+// 		getUserPosts(user.id).then((posts) => {
+// 			console.log(user.name);
+// 			console.log(posts.length);
+// 		});
+// 	});
+// });
 
-{ userId: 1 }
-[[userId, 1]]
-function getFetch(url, params) {
-  const queryString = Object.entries(params).map(param => {
-    return ``
-  })
-  return fetch(url, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json'}
-  }).then(res => res.json())
+{
+	userId: 1;
+}
+[[userId, 1]];
+function getFetch(url, params = {}) {
+	const queryString = Object.entries(params)
+		.map((param) => {
+			return `${param[0]}=${param[1]}`;
+		})
+		.join("&");
+	return fetch(`${url}?${queryString}`, {
+		method: "GET",
+		headers: { "Content-Type": "application/json" },
+	}).then((res) => res.json());
 }
