e6087f295b1d4fbcc6962c0bdc3f0d5db3683a2f
John P <70922864+p134c0d3@users.noreply.github.com>
2023-09-18 21:52:09 -0500

    Update promises.js

--- javascript-advanced-concepts/promises.js
+++ javascript-advanced-concepts/promises.js
@@ -1,14 +1,22 @@
-// global execution context
+// // global execution context
 
-console.log("message");
-logger()
-console.log(1);
+// console.log("message");
+// logger()
+// console.log(1);
 
+// // function execution context
 
+// function logger() {
+//   console.log(2);
+//   console.log(3);
+// }
 
-// function execution context 
-
-function logger() {
-  console.log(2);
-  console.log(3);
+async function getTodos() {
+	const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+	const data = await response.json()
+  console.log(response);
+  console.log(data);
 }
+
+
+getTodos();
